cb0b0e8ae7283edd9f66326aae107b93f59536f2
Kamary <kamarygillespie@Kamarys-MBP.lan>
2022-08-14 19:53:54 -0600

    figured out how to add date and time using moment.js and added an id to each time div that correspons with the military time of that div

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,15 @@
+var date = moment(); // grabs date from moment.js
+var time = moment().hours(); // grabs time from moment js
+
+var dateandTime = date.format("dddd, MMMM Do YYYY, h:mm a"); // formats date on page and time
+$("#currentDay").text(dateandTime); //sets text value of currentDay paragraph to value of Date and Time
+
+console.log(time);
+
 var saveEvent = $(".save-button");
 
-function storeEvent(event) {
+
+saveEvent.on("click", function(event) {
     event.preventDefault();
-    console.log("hello");
-}
-
-
-saveEvent.on("click", storeEvent);
+    console.log("test");
+});
